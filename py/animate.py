21b1f254d28a596c660f71268075da2cc0269774
SebastianJL <johannes.lade@yahoo.com>
2018-04-07 07:06:08 +0200

    add functionality to use latest output

--- py/animate.py
+++ py/animate.py
@@ -8,10 +8,14 @@ import numpy as np
 from scipy.io import FortranFile
 
 
-def directory(parser, arg):
-    '''Check if an argument is a directory and format such that it ends with / '''
+def directory(arg, parser=None):
+    """Check if argument is a directory and format such that it ends with "/" """
     if not os.path.isdir(arg):
-        parser.error('The directory "{}" does not exist'.format(arg))
+        msg = 'The directory "{}" does not exist'.format(arg)
+        try:
+            parser.error(msg)
+        except AttributeError:
+            raise FileNotFoundError(msg)
     else:
         if not arg[-1] == '/':
             arg += '/'
@@ -20,20 +24,27 @@ def directory(parser, arg):
 
 # parse cli arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', type=lambda arg: directory(parser, arg), default='./',
+parser.add_argument('-d', '--directory', type=lambda arg: directory(arg, parser), default='./',
                     help='directory in which output to be processed is saved', dest='dir')
 parser.add_argument('-f', '--filename', type=str, default='animation.gif',
                     help='filename for animation. also determines filetype through ending',
                     dest='filename')
 parser.add_argument('-F', '--format', type=str, default='output_{:05}.00000', help='file format for output files',
                     dest='format')
+parser.add_argument('-l', '--latest', action='store_true', help='attempt to use latest output directory', dest='use_latest')
 args = parser.parse_args()
 
-# path the the file
+# determine path to files
+if args.use_latest:
+    output = '../output/'
+    dirs = (os.path.join(output, dir) for dir in os.listdir(output) if os.path.isdir(os.path.join(output, dir)))
+    path = directory(max(dirs)) + args.format
+else:
+    path = args.dir + args.format
+
 map_files = []
 j = 0
-path = args.dir + args.format
-print("loading files...")
+print('loading files from {}'.format(path))
 while True:
     if not os.path.exists(path.format(j)):
         break
@@ -47,7 +58,6 @@ dpi = 96
 fig = plt.figure(figsize=(800 / dpi, 800 / dpi), dpi=dpi)
 
 print("reading image data from files...")
-# read image data
 frames = []
 for map_file in map_files:
     f = FortranFile(map_file, 'r')
@@ -74,7 +84,6 @@ for map_file in map_files:
 # animate
 ani = animation.ArtistAnimation(fig, frames, interval=100, repeat_delay=100)
 
-# needs ffmpeg to be installed
 # save animation
 print('saving animation...')
 start_time = time.time()
