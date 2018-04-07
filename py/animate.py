811c92f0e8b056aa370478b69a36cc82aa54311f
SebastianJL <johannes.lade@yahoo.com>
2018-04-07 08:16:18 +0200

    corrected english language

--- py/animate.py
+++ py/animate.py
@@ -27,7 +27,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--directory', type=lambda arg: directory(arg, parser), default='./',
                     help='directory in which output to be processed is saved', dest='dir')
 parser.add_argument('-o', '--outfile', type=str, default='animation.mp4',
-                    help='filename for animation. also determines filetype through ending')
+                    help='filename for animation. also determines filetype through file extension')
 parser.add_argument('-f', '--format', type=str, default='output_{:05}.00000', help='file format for output files')
 parser.add_argument('-l', '--latest', action='store_true', help='attempt to use latest output directory',
                     dest='use_latest')
