84de34b8fab7561d52e22088e8951adc29f27119
SebastianJL <johannes.lade@yahoo.com>
2018-06-09 22:01:21 +0200

    Updated run.py to also animate and show animation.

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 make
-mpirun -np 4 hydro
+output='../output/'
+rm ${output}output_*
+nproc=4
+
+command="mpirun -np ${nproc} hydro ../input/input.nml ${output}"
+echo ${command}
+eval ${command}
+
+cd ../analysis
+command="python animate.py -n=${nproc} -d=${output}"
+echo ${command}
+eval ${command}
+
+command="gwenview ${output}animation.gif"
+echo ${command}
+eval ${command}
