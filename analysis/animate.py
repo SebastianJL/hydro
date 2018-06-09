baa21b208d290c60591e063c7d856e2478f1fc70
SebastianJL <johannes.lade@yahoo.com>
2018-06-09 02:03:43 +0200

    Recoded animate.py
    New features:
    - Generates pngs which are combined with ffmpeg. Much faster than
    matplotlib animation and can handle bigger animations due to smaller
    memory requirement.
    - Uses multiprocessing for faster fortran file reading and png writing.

--- analysis/animate.py
+++ analysis/animate.py
@@ -1,13 +1,25 @@
 import argparse
+import itertools as it
 import os
+import re
 import time
+from multiprocessing.pool import Pool
 from pathlib import Path
-from typing import Any
+from typing import Any, Union
 
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
+from ffmpy3 import FFmpeg
 from scipy.io import FortranFile
+
+
+SETTINGS = {
+    'origin': 'lower',
+    'cmap': 'jet',
+    'vmin': -3.308183,
+    'vmax': 1.5684958,
+    'dpi' : 96
+}
 
 
 def directory(arg: Any, parser: argparse.ArgumentParser = None):
@@ -24,8 +36,8 @@ def directory(arg: Any, parser: argparse.ArgumentParser = None):
         return str(arg)
 
 
-def read(filename: str) -> np.ndarray:
-    with FortranFile(filename, 'r') as f:
+def read(filename: Union[str, Path]) -> np.ndarray:
+    with FortranFile(str(filename), 'r') as f:
         [t, gamma] = f.read_reals('f4')
         [nx, ny, nvar, nstep] = f.read_ints('i')
         dat = f.read_reals('f4')
@@ -33,100 +45,92 @@ def read(filename: str) -> np.ndarray:
     return dat.reshape(nvar, ny, nx)
 
 
-output_dir_prefix = 'output-'
-
-# parse cli arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', type=lambda arg: directory(arg, parser), default='./',
-                    help='directory in which output to be processed is saved', dest='dir')
-parser.add_argument('-o', '--outfile', type=str, default='animation.mp4',
-                    help='filename for animation. also determines filetype through file extension (only mp4 fully \
-                    supported')
-parser.add_argument('-f', '--format', type=str, default='output_{:05}.{:05}', help='file format for output files')
-parser.add_argument('-l', '--latest', action='store_true',
-                    help='attempt to use latest output directory based on lexicographical order. directory name has to \
-                    start with "{}". animation is saved in said output directory'.format(output_dir_prefix),
-                    dest='use_latest')
-parser.add_argument('-s', '--show', action='store_true',
-                    help='show animation after saving. default when using option --no-save  ')
-parser.add_argument('-n', '--no-save', action='store_false', help="don't save animation, show instead", dest='save')
-args = parser.parse_args()
-
-# determine path to files
-if args.use_latest:
-    output = Path('../output/')
-    dirs = (dir for dir in output.iterdir() if dir.name.startswith(output_dir_prefix))
-    try:
-        dir = max(dirs)
-    except ValueError as e:
-        if e.args[0] == 'max() arg is an empty sequence':
-            raise FileNotFoundError(
-                'No directory with prefix "{}" found. Use --directory to specifiy location of directory with output files.'.format(
-                    output_dir_prefix))
-        else:
-            raise
-    path = dir / args.format
-    args.outfile = dir / 'animation.mp4'
-else:
-    dir = Path(args.dir)
-    path = dir / args.format
-print('file format pattern: {}'.format(path))
-
-# determine num_cpu
-num_cpu = 0
-while True:
-    if not os.path.exists(str(path).format(0, num_cpu)):
-        break
-    num_cpu += 1
-print('found files from {} cpus'.format(num_cpu))
-
-# define figure
-dpi = 96
-fig = plt.figure(figsize=(800 / dpi, 800 / dpi), dpi=dpi)
-
-# read files
-print("reading image data from files...")
-frames = []
-settings = {
-    'interpolation': 'nearest',
-    'origin': 'lower',
-    'animated': True,
-    'cmap': 'jet',
-    'vmin': -3.308183,
-    'vmax': 1.5684958
-}
-j = 0
-while os.path.exists(str(path).format(j, 0)):
-    master_file = str(path).format(j, 0)
+def write_png_file(master_file: Path) -> None:
     master_data = read(master_file)
     for i in range(1, num_cpu):
-        slave_file = str(path).format(j, i)
-        slave_data = read(slave_file)
-        master_data = np.hstack((master_data, slave_data))
+        slave_file = master_file.parent / '{}.{:05}'.format(master_file.name.split(".")[0], i)
+        if slave_file in data_files:
+            slave_data = read(slave_file)
+            master_data = np.hstack((master_data, slave_data))
 
-    # plot the map
-    frame = np.log10(master_data[0, :, :])
-    img = plt.imshow(frame, **settings)
-    frames.append([img])
-    j += 1
-
-if j == 0:
-    print("no files found, exiting...")
-    exit()
+    number = master_file.name.partition('_')[2].partition('.')[0]
+    file = master_file.parent / 'frame_{}.png'.format(number)
+    plt.imsave(str(file), np.log10(master_data[0, :, :]), **SETTINGS)
 
 
+if __name__ == '__main__':
+    output_dir_prefix = 'output-'
 
-# animate
-ani = animation.ArtistAnimation(fig, frames, interval=100, repeat_delay=100)
+    # parse cli arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', type=lambda arg: directory(arg, parser), default='./',
+                        help='directory in which output to be processed is saved.', dest='dir')
+    parser.add_argument('-o', '--outfile', type=str, default='animation.gif',
+                        help='filename for animation. (must be compatible with ffmpeg.)')
+    parser.add_argument('-l', '--latest', action='store_true',
+                        help='attempt to use latest output directory based on lexicographical order. directory name has to \
+                        start with "{}". animation is saved in said output directory.'.format(output_dir_prefix),
+                        dest='use_latest')
+    parser.add_argument('-n', '--nproc', type=int, default=1, help='number of processes for multiprocessing.')
+    parser.add_argument('--read', dest='read', action='store_true', help='read Fortran Files and save to png files.')
+    parser.add_argument('--no-read', dest='read', action='store_false', help="don't read Fortran Files and save to png files.")
+    parser.set_defaults(read=True)
+    args = parser.parse_args()
 
-# save animation
-if args.save:
-    print('saving animation in {}'.format(args.outfile))
-    start_time = time.time()
+    # determine path to files
+    wall_time = time.time()
+    if args.use_latest:
+        output = Path('../output/')
+        dirs = (dir for dir in output.iterdir() if dir.name.startswith(output_dir_prefix))
+        try:
+            dir_ = max(dirs)
+        except ValueError as e:
+            if e.args[0] == 'max() arg is an empty sequence':
+                raise FileNotFoundError(
+                    'No directory with prefix "{}" found. Use --directory to specifiy location of directory with output files.'.format(
+                        output_dir_prefix))
+            else:
+                raise
+    else:
+        dir_ = Path(args.dir)
 
-    ani.save(args.outfile, writer=animation.FFMpegWriter(fps=30, codec='libx264'))
-    print('time needed for saving: {:.2f}s'.format(time.time() - start_time))
+    outfile = dir_ / args.outfile
 
-# display
-if args.show or not args.save:
-    plt.show()
+    if args.read:
+        # find files
+        data_files = [f for f in dir_.iterdir() if re.match(r'output_[0-9]{5}.[0-9]{5}', f.name)]
+        data_files.sort()
+        master_files = [f for f in data_files if re.match(r'output_[0-9]{5}.[0]{5}', f.name)]
+        master_files.sort()
+        num_cpu = len(list(it.takewhile(lambda f: re.match(r'output_[0]{5}.[0-9]{5}', f.name), data_files)))
+        if not data_files:
+            print("no files found, exiting...")
+            exit()
+
+        # delete old pngs
+        for file in dir_.glob('*.png'):
+            file.unlink()
+
+        # read output files and save as png files
+        read_write_time = time.time()
+        print("reading image data from files and saving to png...")
+        with Pool(processes=args.nproc) as pool:
+            data = pool.map(write_png_file, master_files)
+        read_write_time = time.time() - read_write_time
+
+    # convert png files to animation
+    print('converting to animation in {}'.format(outfile))
+    convert_time = time.time()
+
+    ff = FFmpeg(
+        inputs={str(dir_ / 'frame_%5d.png'): None},
+        outputs={str(outfile): '-loglevel error -y -nostats'}
+    )
+    print(ff.cmd)
+    ff.run()
+    convert_time = time.time() - convert_time
+
+    if args.read:
+        print('time needed for read/write pngs: {:.2f}s'.format(read_write_time))
+    print('time needed for converting: {:.2f}s'.format(convert_time))
+    print('wall time: {:.2f}s'.format(time.time() - wall_time))
